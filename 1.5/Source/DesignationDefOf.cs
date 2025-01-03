222e77028216a0dca805ad94819d24b67c61283c
Taranchuk <nfhfyxer@gmail.com>
2025-01-03 19:39:44 +0200

    designator

---
+++ 1.5/Source/DesignationDefOf.cs
@@ -0,0 +1,11 @@
+using RimWorld;
+using Verse;
+
+namespace DeathSentence
+{
+    [DefOf]
+    public static class DesignationDefOf
+    {
+        public static DesignationDef ExecutionOrder;
+    }
+}
